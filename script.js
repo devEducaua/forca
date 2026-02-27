81607b48a5223373b490e81547584c9e590816c6
devEducaua <schokito4@gmail.com>
2026-02-27 17:39:10 -0300

    feat: substitute array for api to get words

--- script.js
+++ script.js
@@ -3,31 +3,54 @@ const attemptsIndicator = document.getElementById("attempts-indicator");
 const keys = document.getElementsByClassName("key");
 const newGameBtn = document.getElementById("newgame");
 
-const wordList = [
-    "maca","banana","laranja","uva","melancia","melao","pessego","pera","ameixa","cereja",
-    "manga","limao","lima","abacaxi","coco","kiwi","morango","framboesa","amora","mirtilo",
-    "carro","caminhao","bicicleta","moto","aviao","trem","navio","barco","onibus","metro", "marimbondo",
-    "rua","estrada","ponte","tunel","cidade","vila","bairro","pais","capital","estado", "molibdenio", "olheiro",
-    "casa","apartamento","predio","escritorio","escola","universidade","biblioteca","museu","hospital","mercado",
-    "computador","teclado","mouse","tela","celular","tablet","camera","altofalante","microfone","impressora",
-    "livro","papel","caneta","lapis","caderno","pasta","documento","carta","mensagem","email", "jaguara",
-    "sol","lua","estrela","ceu","mar","rio","montanha","floresta","chuva","vento", "guri", "gaucho"
-]
+// const wordList = [
+//     "maca","banana","laranja","uva","melancia","melao","pessego","pera","ameixa","cereja",
+//     "manga","limao","lima","abacaxi","coco","kiwi","morango","framboesa","amora","mirtilo",
+//     "carro","caminhao","bicicleta","moto","aviao","trem","navio","barco","onibus","metro", "marimbondo",
+//     "rua","estrada","ponte","tunel","cidade","vila","bairro","pais","capital","estado", "molibdenio", "olheiro",
+//     "casa","apartamento","predio","escritorio","escola","universidade","biblioteca","museu","hospital","mercado",
+//     "computador","teclado","mouse","tela","celular","tablet","camera","altofalante","microfone","impressora",
+//     "livro","papel","caneta","lapis","caderno","pasta","documento","carta","mensagem","email", "jaguara",
+//     "sol","lua","estrela","ceu","mar","rio","montanha","floresta","chuva","vento", "guri", "gaucho"
+// ]
 
-function randomWord() {
-    const random = wordList[Math.floor(Math.random() * wordList.length)];
-    return random;
+async function getRandomWord() {
+    const url = "https://api.dicionario-aberto.net/random";
+    const resp = await fetch(url);
+    const result = await resp.json();
+    return parseWord(result.word);
+}
+
+function parseWord(word) {
+    let parsedWord = "";
+    const blockedLetters = new Map([
+        ["á", "a"], ["é", "e"], ["í", "i"], ["ó", "o"], ["ú", "u"], ["ç", "c"]
+    ]);
+
+    for (let i = 0; i < word.length; i++) {
+        let l = word[i];
+        if (blockedLetters.get(word[i]) != undefined) {
+            l = blockedLetters.get(word[i]);
+        }
+
+        parsedWord += l;
+    }
+
+    return parsedWord;
 }
 
 let attempts = 6;
-let letters = randomWord().split("");
+let word = await getRandomWord();
+let letters = word.split("");
+// let letters = await (getRandomWord().split(""));
 let hiddenWord = new Array(letters.length).fill("_");
 wordIndicator.textContent = hiddenWord.join("");
 
-newGameBtn.addEventListener("click", () => {
+newGameBtn.addEventListener("click", async () => {
     attempts = 6;
     attemptsIndicator.textContent = attempts;
-    letters = randomWord().split("");
+    let word = await getRandomWord();
+    letters = word.split("");
     hiddenWord = new Array(letters.length).fill("_");
     wordIndicator.textContent = hiddenWord.join("");
 
